959180fad17389a7a6eeade907143c41856d605e
NicolasBreels <nicolas.breels@gmail.com>
2023-02-07 16:02:29 +0100

    React: 2.1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Banner from './composants/Banner'
+import Cart from './composants/Cart'
+
+function App() {
+    return		<div>
+              <Banner />
+              <Cart />
+              </div>
+}
+
+export default App
